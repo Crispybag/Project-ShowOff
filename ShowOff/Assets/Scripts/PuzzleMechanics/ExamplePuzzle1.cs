5abf992ed1309299e768766b70bde81737876f1b
Lylithrea <ezraduss@gmail.com>
2021-05-10 16:22:44 +0200

    Added door conditions, and puzzle factory

---
+++ ShowOff/Assets/Scripts/PuzzleMechanics/ExamplePuzzle1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExamplePuzzle1 : PuzzleManager
+{
+    public override void FinishMechanic()
+    {
+        isCompleted = true;
+    }
+}
