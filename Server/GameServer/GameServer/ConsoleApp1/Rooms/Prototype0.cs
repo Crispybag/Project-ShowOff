6c3708d933ef31e1463bd7763d13597fe2251909
Lylithrea <ezraduss@gmail.com>
2021-05-17 16:54:49 +0200

    Added movement to server, doesnt work perfectly

---
+++ Server/GameServer/GameServer/ConsoleApp1/Rooms/Prototype0.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using sharedAngy;
+
+namespace Server
+{
+    class Prototype0 : Room
+    {
+        public Prototype0(TCPGameServer pServer) : base(pServer) 
+        {
+        }
+
+        #region Server startup
+        public void AddMember(TCPMessageChannel pChannel)
+        {
+            Console.WriteLine("User Joined test room");
+            addMember(pChannel);
+        }
+
+        private void handleReqJoinServer(ReqJoinServer reqJoinServer, TCPMessageChannel pSender)
+        {
+
+            ConfJoinServer joinMessage = new ConfJoinServer();
+            foreach (PlayerInfo pInfo in _server._allConnectedUsers.Values)
+            {
+
+                if (reqJoinServer.requestedName == pInfo.GetPlayerName())
+                {
+                    joinMessage.acceptStatus = false;
+                    pSender.SendMessage(joinMessage);
+                    removeAndCloseMember(pSender);
+                    return;
+                }
+
+            }
+            joinMessage.acceptStatus = true;
+            pSender.SendMessage(joinMessage);
+            _server.AddPlayerInfo(pSender, reqJoinServer.requestedName);
+
+        }
+
+        #endregion
+
+        protected override void handleNetworkMessage(ASerializable pMessage, TCPMessageChannel pSender)
+        {
+            try
+            {
+                if (pMessage is ReqJoinServer)
+                {
+                    handleReqJoinServer(pMessage as ReqJoinServer, pSender);
+                }
+                if (pMessage is ReqMove)
+                {
+                    handleReqMove(pMessage as ReqMove);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("A package has been send but we dont know how to handle this!");
+            }
+
+        }
+
+        private void handleReqMove(ReqMove pMove)
+        {
+            ConfMove confMove = new ConfMove();
+            confMove.name = pMove.name;
+            confMove.dirX = pMove.dirX;
+            confMove.dirY = pMove.dirY;
+            confMove.dirZ = pMove.dirZ;
+            sendToAll(confMove);
+        }
+
+
+    }
+}
--- ShowOff/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ ShowOff/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -2,24 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static ServiceLocator;
+using sharedAngy;
+using System.Net.Sockets;
+
 public class PlayerMovement : Movement
 {
     [SerializeField] private float _moveSpeed;
     private InputManager _inputManager;
     public int playerPushWeight;
 
+    [Tooltip("This will be the name in the service locator!")]public string playerName = "Player1";
+    [SerializeField] private BasicTCPClient basicClient;
 
     private void Awake()
     {
-        serviceLocator.AddToList("Player1", this.gameObject);
+        serviceLocator.AddToList(playerName, this.gameObject);
     }
 
 
     protected override void Start()
     {
         base.Start();
-/*        canFall = true;
-        _travelTime = _moveSpeed;*/
         _inputManager = serviceLocator.GetFromList("InputManager").GetComponent<InputManager>();
     }
     // Update is called once per frame
@@ -29,17 +32,35 @@ public class PlayerMovement : Movement
         base.Update();
         if (_inputManager.GetAction(InputManager.Action.HORIZONTAL))
         {
-            moveToTile(new Vector3(_inputManager.getHorizontalInput(),0,0));
+            ReqMove newMove = new ReqMove();
+            newMove.name = playerName;
+            newMove.dirX = _inputManager.getHorizontalInput();
+            newMove.dirY = 0;
+            newMove.dirZ = 0;
+            sendPackage(newMove);
+            //moveToTile(new Vector3(_inputManager.getHorizontalInput(),0,0));
         }
 
         if (_inputManager.GetAction(InputManager.Action.VERTICAL))
         {
-            moveToTile(new Vector3(0, 0, _inputManager.getVerticalInput()));
+            ReqMove newMove = new ReqMove();
+            newMove.name = playerName;
+            newMove.dirX = 0;
+            newMove.dirY = 0;
+            newMove.dirZ = _inputManager.getVerticalInput();
+            sendPackage(newMove);
+            //moveToTile(new Vector3(0, 0, _inputManager.getVerticalInput()));
         }
         //this allows other objects to move this object. (Boxes now can move the player, useful for airchannels)
         if (wallCheckCalled)
         {
-            moveToTile(_direction);
+            //moveToTile(_direction);
+            ReqMove newMove = new ReqMove();
+            newMove.name = playerName;
+            newMove.dirX = (int)_direction.x;
+            newMove.dirY = (int)_direction.y;
+            newMove.dirZ = (int)_direction.z;
+            sendPackage(newMove);
         }
         wallCheckCalled = false;
 
@@ -59,4 +80,12 @@ public class PlayerMovement : Movement
         return isWall;
     }
 
+    private void sendPackage(ASerializable pSerializable)
+    {
+        //create the packet
+        Packet _outPacket = new Packet();
+        _outPacket.Write(pSerializable);
+        //send package to the stream
+        StreamUtil.Write(basicClient._client.GetStream(), _outPacket.GetBytes());
+    }
 }
