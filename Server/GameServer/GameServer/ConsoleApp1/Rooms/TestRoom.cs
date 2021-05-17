1964c78174ff46577feac95e5e436ca594511e13
BuildTools <crispybag@gmail.com>
2021-05-17 14:24:31 +0200

    full server push

---
+++ Server/GameServer/GameServer/ConsoleApp1/Rooms/TestRoom.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using sharedAngy;
+
+namespace Server
+{
+    class TestRoom : Room
+    {
+        public TestRoom(TCPGameServer pServer) : base(pServer) { }
+        private int _counter;
+        protected override void handleNetworkMessage(ASerializable pMessage, TCPMessageChannel pSender)
+        {
+            if (pMessage is ReqAddCount)
+            {
+                Console.WriteLine("Received a Request Add Count");
+                handleReqAddCount();
+            }
+            if (pMessage is ReqMove)
+            {
+                handleReqMove(pMessage as ReqMove);
+            }
+        }
+        public void AddMember(TCPMessageChannel pChannel)
+        {
+            Console.WriteLine("User Joined test room");
+            addMember(pChannel);
+        }
+
+        private void handleReqAddCount()
+        {
+            _counter++;
+            ConfAddCount addCount = new ConfAddCount();
+            addCount.totalCount = _counter;
+            sendToAll(addCount);
+        }
+
+        private void handleReqMove(ReqMove pMove)
+        {
+            ConfMove confMove = new ConfMove();
+            confMove.dirX = pMove.dirX;
+            confMove.dirY = pMove.dirY;
+            confMove.dirZ = pMove.dirZ;
+            sendToAll(confMove);
+
+        }
+    }
+}
---
+++ Server/GameServer/GameServer/sharedAngy/DataStructure/PacketFormats/Actuators/ReqActuatorToggle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharedAngy
+{
+    public class ReqActuatorToggle : ASerializable
+    {
+        public bool isActivated;
+        public override void Deserialize(Packet pPacket)
+        {
+            isActivated = pPacket.ReadBool();
+        }
+
+        public override void Serialize(Packet pPacket)
+        {
+            pPacket.Write(isActivated);
+        }
+    }
+}
