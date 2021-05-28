1ff3c2f02cc4109e947bdac214bb026ae75aa1ea
BuildTools <crispybag@gmail.com>
2021-05-28 10:19:57 +0200

    I forgot to run the server to save everything

--- Server/GameServer/GameServer/ConsoleApp1/Game Elements/GameObject.cs
+++ Server/GameServer/GameServer/ConsoleApp1/Game Elements/GameObject.cs
@@ -24,7 +24,7 @@ namespace Server
         }
 
         
-        protected CollInteractType collState;
+        public CollInteractType collState;
 
         public GameRoom room;
         //First initialise
@@ -43,12 +43,73 @@ namespace Server
             collState = pNewState;
         }
 
+        #region movement tools
+        public void MoveDirection(int pX, int pY, int pZ)
+        {
+            //change the location of the player and remove the player value from the grid at the place the player was
+            room.OnCoordinatesRemove(x(), y(), z(), objectIndex);
+            position[0] += pX;
+            position[1] += pY;
+            position[2] += pZ;
+            //add
+            room.roomArray[x(), y(), z()].Add(objectIndex);
+            Logging.LogInfo("Player's position is now ( " + position[0] + ", " + position[1] + ", " + position[2] + ")", Logging.debugState.DETAILED);
+        }
+        public void MoveDirection(int[] pDirection)
+        {
+            try
+            {
+                //change the location of the player and remove the player value from the grid at the place the player was
+                room.OnCoordinatesRemove(x(), y(), z(), objectIndex);
+                position[0] += pDirection[0];
+                position[1] += pDirection[1];
+                position[2] += pDirection[2];
+                //add
+                room.roomArray[x(), y(), z()].Add(objectIndex);
+                Logging.LogInfo("Player's position is now ( " + position[0] + ", " + position[1] + ", " + position[2] + ")", Logging.debugState.DETAILED);
+            }
+            catch
+            {
+                Logging.LogInfo("index was outside of bounds in move direction in gameobject", Logging.debugState.SIMPLE);
+            }
+        }
+        public void MovePosition(int pX, int pY, int pZ)
+        {
+            //change the location of the player and remove the player value from the grid at the place the player was
+            room.OnCoordinatesRemove(x(), y(), z(), objectIndex);
+            position[0] = pX;
+            position[1] = pY;
+            position[2] = pZ;
+            //add
+            room.roomArray[x(), y(), z()].Add(objectIndex);
+            Logging.LogInfo("Player's position is now ( " + position[0] + ", " + position[1] + ", " + position[2] + ")", Logging.debugState.DETAILED);
+        }
+        public void MovePosition(int[] pPosition)
+        {
+            try
+            {
+                //change the location of the player and remove the player value from the grid at the place the player was
+                room.OnCoordinatesRemove(x(), y(), z(), 1);
+                position[0] = pPosition[0];
+                position[1] = pPosition[1];
+                position[2] = pPosition[2];
+                //add
+                room.roomArray[x(), y(), z()].Add(1);
+                Logging.LogInfo("Player's position is now ( " + position[0] + ", " + position[1] + ", " + position[2] + ")", Logging.debugState.DETAILED);
+            }
+            catch
+            {
+                Logging.LogInfo("index was outside of bounds in move direction in gameobject", Logging.debugState.SIMPLE);
+            }
+        }
+        #endregion
 
         //override update void for children to use
         public virtual void Update()
         {
 
         }
+
 
     }
 }
