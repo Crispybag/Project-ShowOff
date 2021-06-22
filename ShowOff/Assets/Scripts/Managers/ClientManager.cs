44f9967beedbaaeff6e6953c0879774537989607
Lylithrea <ezraduss@gmail.com>
2021-06-22 15:47:03 +0200

    Added crack fix tool

--- ShowOff/Assets/Scripts/Managers/ClientManager.cs
+++ ShowOff/Assets/Scripts/Managers/ClientManager.cs
@@ -22,7 +22,12 @@ public class ClientManager : MonoBehaviour
     public string ClientName;
     [HideInInspector] public int playersWantToReset = 0;
     public string gameSceneName = "Features";
+    
+    
     private bool isHoldingBox = false;
+    private GameObject box;
+    private bool isHandlingBox;
+    private float timer;
 
     //----------------------- private ------------------------
 
@@ -53,6 +58,19 @@ public class ClientManager : MonoBehaviour
     void Update()
     {
         receiveInCommingData();
+        if (isHandlingBox)
+        {
+            if(timer > 0.65f)
+            {
+                timer = 0;
+                box.SetActive(!isHoldingBox);
+                isHandlingBox = false;
+            }
+            else
+            {
+                timer += Time.deltaTime;
+            }
+        }
     }
 
     //=========================================================================================
@@ -103,7 +121,6 @@ public class ClientManager : MonoBehaviour
         }
     }
 
-
     private void handleConfAnimation(ConfAnimation pMessage)
     {
         GameObject player1 = serviceLocator.GetFromList("Player1");
@@ -136,7 +153,6 @@ public class ClientManager : MonoBehaviour
         }
     }
 
-
     private void handleConfPlayerSwitch(ConfPlayerSwitch pMessage)
     {
         FindObjectOfType<HandleCharacterSelection>().UpdateCharacters(pMessage.playerIndex);
@@ -166,7 +182,6 @@ public class ClientManager : MonoBehaviour
             sceneManager.LoadSceneSingle(pMessage.sceneName);
         }
     }
-
 
     private void handleReloadScene(ConfReloadScene pMessage)
     {
@@ -299,6 +314,7 @@ public class ClientManager : MonoBehaviour
                 break;
         }
     }
+   
     private void handleConfMove(ConfMove pMessage)
     {
         GameObject player1 = serviceLocator.GetFromList("Player1");
@@ -369,16 +385,24 @@ public class ClientManager : MonoBehaviour
         {
             Debug.Log("Got a box with a value of" + pHandleBox.isPickedUp);
             serviceLocator.interactableList[pHandleBox.ID].GetComponent<BoxMovement>().UpdateBox(pHandleBox.isPickedUp, pHandleBox.posX, pHandleBox.posY, pHandleBox.posZ);
+            //serviceLocator.interactableList[pHandleBox.ID].SetActive(!pHandleBox.isPickedUp);
+            box = serviceLocator.interactableList[pHandleBox.ID];
+            
             if (pHandleBox.isPickedUp != isHoldingBox)
             {
                 isHoldingBox = pHandleBox.isPickedUp;
                 if (isHoldingBox)
                 {
-                    serviceLocator.GetFromList(ClientName).GetComponentInChildren<AnimationHandler>().DoTrigger("PickUp");
+                    Debug.Log("Pick Animation");
+                    serviceLocator.GetFromList(ClientName).GetComponent<AnimationHandler>().DoTrigger("PickUp");
+                    isHandlingBox = true;
+                    
                 }
                 else if (!isHoldingBox)
                 {
-                    serviceLocator.GetFromList(ClientName).GetComponentInChildren<AnimationHandler>().DoTrigger("DropOff");
+                    Debug.Log("Drop Animation");
+                    serviceLocator.GetFromList(ClientName).GetComponent<AnimationHandler>().DoTrigger("DropOff");
+                    isHandlingBox = true;
                 }
             }
         }
@@ -387,6 +411,8 @@ public class ClientManager : MonoBehaviour
             Debug.LogError("Could not find box movement");
         }
     }
+
+
 
     #region ReadIncommingData
 
